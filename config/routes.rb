65f01c450b8d16aa6e25e189b0a0545c77edbee9
cdallasanta <chris.dallasanta@gmail.com>
2020-04-29 16:32:56 -0700

    static pages routing fixed

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # mount ActionCable.server => '/cable'
 
-  get '*path', to: "static_pages#fallback_index_html", constraints: ->(request) do
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
 end
