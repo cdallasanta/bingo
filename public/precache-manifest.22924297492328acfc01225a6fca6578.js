3afabfa0b4719a185b16bb9cbf470c1ce44a466d
cdallasanta <chris.dallasanta@gmail.com>
2020-04-29 16:30:37 -0700

    client packaged

---
+++ public/precache-manifest.22924297492328acfc01225a6fca6578.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ef7f76ca27fd1475e8faf10ab2698f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c106889d34a8a8362fd",
+    "url": "/static/css/main.48133d9f.chunk.css"
+  },
+  {
+    "revision": "66425f0169fe571fd1d5",
+    "url": "/static/js/2.207d1ed5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.207d1ed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c106889d34a8a8362fd",
+    "url": "/static/js/main.5ccc9379.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  },
+  {
+    "revision": "5af66b61f56574b0776c5d75c8c3cacb",
+    "url": "/static/media/fingernail.5af66b61.png"
+  },
+  {
+    "revision": "da1a1de0bc1bf700ebcd24cb1557293a",
+    "url": "/static/media/heart.da1a1de0.png"
+  },
+  {
+    "revision": "8339d0f7117fec6cc85f02d35d7182f1",
+    "url": "/static/media/shamrock.8339d0f7.png"
+  },
+  {
+    "revision": "e250142bad22edcbeb7519c50a72befe",
+    "url": "/static/media/star.e250142b.png"
+  },
+  {
+    "revision": "9215e1d697561a88ad991525916257ca",
+    "url": "/static/media/wine.9215e1d6.png"
+  }
+]);
