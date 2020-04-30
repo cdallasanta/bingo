d2e8aaa9ad1035b6a62d2f3d1b04fbc32d1c344a
cdallasanta <chris.dallasanta@gmail.com>
2020-04-30 12:16:16 -0700

    media queries go at the end for a reason

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,4 +1,5 @@
 export const API_ROOT = "https://bingo-bango-bongo.herokuapp.com/api"
+// export const API_ROOT = "http://localhost:3001/api"
 export const API_WS_ROOT = "ws://localhost:3001/cable"
 export const HEADERS = {
   'Content-Type': 'application/json',
