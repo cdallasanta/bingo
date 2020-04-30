aee7e309d688937775d94c0ba963705f714e1f61
cdallasanta <chris.dallasanta@gmail.com>
2020-04-30 11:02:29 -0700

    tweaked the way games are displayed

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,5 +1,5 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :drawn_numbers, :cards
+  attributes :id, :drawn_numbers, :cards, :created_at
 
   def cards
     object.cards.sort_by{|c| c.id}
